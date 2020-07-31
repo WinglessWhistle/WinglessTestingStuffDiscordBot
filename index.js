ab39f07985a630bcb8fbff5acaf225195e05fbc2
WinglessWhistle <winglesswhistle@gmail.com>
2020-07-31 09:25:37 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Literally just here to fill in the empty page from hosting WTSBot'));
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+
+// ================= START BOT CODE ===================
+const Discord = require('discord.js');
+const client = new Discord.Client();
+client.on("ready", () => {
+    client.user.setPresence({
+        game: { 
+            name: 'my code',
+            type: 'playing'
+        },
+        status: 'dnd'
+    })
+})
+
+// Bot Commands
+
+client.on('message', msg => {
+  if (msg.content === '!ping') {
+    msg.reply('pong!');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === '!dmtest') {
+    message.author.send("test"),
+    msg.reply('DM Sent?');
+  }
+});
+
+
+// Bot Token
+
+client.login(process.env.DISCORD_TOKEN);
