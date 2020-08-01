79c8c0c3fa69486235945cfc2eb7deeb37bb580c
WinglessWhistle <winglesswhistle@gmail.com>
2020-08-01 06:09:39 +0000

    added a new join alert

--- index.js
+++ index.js
@@ -21,30 +21,41 @@ client.on("ready", () => {
 
 // Bot Commands
 
+// !ping
 client.on('message', msg => {
   if (msg.content === '!ping') {
     msg.reply('pong!');
   }
 });
 
+// !invite
 client.on('message', msg => {
   if (msg.content === '!invite') {
     msg.reply('Soon™');
   }
 });
 
+
+// !winglessentials
 client.on('message', msg => {
   if (msg.content === '!winglessentials') {
     msg.reply('Winglessentials is a minecraft plugin recoded and aimed to replace the popular plugin Essentials and EssentialsX. Learn more about it by visiting the GitHub repo - https://github.com/WinglessWhistle/Winglessentials');
   }
 });
 
+// !avatar
 client.on('message', message => {
   if (message.content === '!avatar') {
     message.reply(message.author.displayAvatarURL());
   }
 });
 
+// new join
+client.on('guildMemberAdd', member => {
+  const channel = member.guild.channels.cache.find(ch => ch.name === 'general');
+  if (!channel) return;
+  channel.send(`Welcome to the server, ${member}`);
+});
 
 // Bot Token
 
